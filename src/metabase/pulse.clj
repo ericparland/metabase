565f315041ba3f06bb38ac6d8113c6af6134d388
Eric Parland <erik.parland@nordigy.ru>
2016-12-14 21:34:21 +0300

    Merging together  Impala (https://github.com/metabase/metabase/pull/3749/files) and Vertica (https://github.com/metabase/metabase/pull/3387/) support

--- src/metabase/pulse.clj
+++ src/metabase/pulse.clj
@@ -79,8 +79,8 @@
   (glip/regenerate-cookie)
   (doall (for [{{card-id :id, card-name :name, :as card} :card, result :result} results]
             (let [image-byte-array (render/render-pulse-card-to-png card result)]
-              (glip/upload-and-post-file! image-byte-array "image.png")))
-  ;(glip/post-chat-message!  (str "Pulse: " (:name pulse)))
+              (glip/upload-and-post-file! (:_id (get (#(zipmap (map :set_abbreviation %) %)(glip/groups-list)) channel-id)) image-byte-array "image.png")))
+              (glip/post-chat-message! (:_id (get (#(zipmap (map :set_abbreviation %) %)(glip/groups-list)) channel-id)) (str "Pulse: " (:name pulse)))
          )
     ;)
   )
