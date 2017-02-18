52c95a9e62286f1f7dd9f73e528ce326f7a7139f
erik.parland <erik.parland@nordigy.ru>
2017-02-17 16:45:45 -0800

    Adding title card to pulses

--- src/metabase/pulse.clj
+++ src/metabase/pulse.clj
@@ -78,7 +78,7 @@
   (log/debug (u/format-color 'cyan "Sending Pulse (%d: %s) via Glip"  (:id pulse) (:name pulse)))
   (glip/regenerate-cookie)
   (doall (for [{{card-id :id, card-name :name, :as card} :card, result :result} results]
-            (let [image-byte-array (render/render-pulse-card-to-png card result)]
+            (let [image-byte-array (binding [render/*include-title* true](render/render-pulse-card-to-png card result))]
               (log/warn "Rendered card: " (u/pprint-to-str image-byte-array))
               (glip/upload-and-post-file! (:_id (get (#(zipmap (map :set_abbreviation %) %)(glip/groups-list)) channel-id)) image-byte-array "image.png")
               (glip/post-chat-message! (:_id (get (#(zipmap (map :set_abbreviation %) %)(glip/groups-list)) channel-id)) (str "Pulse: " (:name pulse)))
